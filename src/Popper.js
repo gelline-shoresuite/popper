f72b3fd44e1c755bac398d425ade38368221c5f9
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-06 11:47:09 +0800

    Added Popper.js

---
+++ src/Popper.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Popper extends Component{
+  constructor(){
+    super();
+  
+    this.state={
+
+    }
+    
+  }
+
+  render(){
+    
+  }
+}
+
+export default Popper;
