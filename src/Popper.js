63f4dac4089e123302c6a8fcb7b3de1a6490819d
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-06 12:59:09 +0800

    Logic worked!

--- src/Popper.js
+++ src/Popper.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
+import './Popper.css'
+
 class Popper extends Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
   
     this.state={
-
+      active: false
     }
-    
+
+  }
+
+  toggleClass = () =>{
+    const currentState = this.state.active;
+    this.setState({active: !currentState});
+
+    console.log(currentState)
   }
 
   render(){
-    
+    return(
+      <div key={this.props.popperKey}>
+        <div className={(this.state.active? 'active': null) + ' poppers'}>{this.props.popupText}</div>
+        <button onClick={this.toggleClass} className='theButton'>CLICK CLICK Me</button>
+      </div>
+
+    )
   }
 }
 
