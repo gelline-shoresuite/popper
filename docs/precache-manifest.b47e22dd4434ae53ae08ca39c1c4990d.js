1be8852dbcf0f1ea63938366d3080146187320e3
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-06 14:16:49 +0800

    Added /docs which contains the build files

---
+++ docs/precache-manifest.b47e22dd4434ae53ae08ca39c1c4990d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f15ed52424d1b750ae49",
+    "url": "/static/css/main.1a45c559.chunk.css"
+  },
+  {
+    "revision": "f15ed52424d1b750ae49",
+    "url": "/static/js/main.f15ed524.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ccc79888c4c678405b3b1b53e92342a",
+    "url": "/index.html"
+  }
+];
